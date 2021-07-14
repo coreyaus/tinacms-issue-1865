da77710484d2f35202ad3b850c330a58189a0081
Corey M <corey@codenation.com>
2021-07-14 12:17:17 +1000

    Add react-tinacms-editor & react-tinacms-inline
    - Run `yarn add react-tinacms-editor react-tinacms-inline`
    - Pass a cms instance to TinaCloudProvider, but without any plugins for now
    - NOTE: I also changed the name of the first post from `HelloWorld.md` to `helloworld.md`, which was necessary to avoid 404 errors at the http://localhost:3000/demo/blog/helloworld route in my dev environment
    - Everything still works at this point when accessing: http://localhost:3000/demo/blog/helloworld

--- components/tina-wrapper.tsx
+++ components/tina-wrapper.tsx
@@ -1,9 +1,24 @@
 
 import { TinaCloudProvider, useGraphqlForms } from "tina-graphql-gateway";
+import { useMemo } from "react";
+import { TinaCMS } from "tinacms";
+import { LocalClient } from "tina-graphql-gateway";
+// import { MarkdownFieldPlugin } from "react-tinacms-editor";
 
 const TinaWrapper = (props) => {
+  const cms = useMemo(() => {
+    return new TinaCMS({
+      apis: {
+        tina: new LocalClient(),
+      },
+      // plugins: [MarkdownFieldPlugin],
+      enabled: true,
+    });
+  }, []);
+
   return (
     <TinaCloudProvider
+      cms={cms}
       clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
       branch={process.env.NEXT_PUBLIC_EDIT_BRACH}
       organization={process.env.NEXT_PUBLIC_ORGANIZATION_NAME}
